b1e36e966be469b1300d7bb26abd0fa5e71b51f8
spencejax21 <spencejax21@gmail.com>
2023-02-27 13:59:32 -0500

    Add logout button

--- login/models.py
+++ login/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    def __str__(self):
+        return self.email
+    email = models.CharField(max_length=200)
+    is_admin = models.BooleanField(default=False)
