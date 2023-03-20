48f74391f6d08410468c549adf1a36c9d6c14a29
spencejax21 <spencejax21@gmail.com>
2023-03-20 16:29:59 -0400

    Added initial tests for SEAS page

--- login/tests.py
+++ login/tests.py
@@ -1,4 +1,29 @@
 from django.test import TestCase
+from .models import Message, Offering
 
 # Create your tests here.
+class SeasTestCase(TestCase):
+
+    def test_seas_results(self):
+        response = self.client.get('/home/seas/results')
+        self.assertEqual(response.status_code, 200)
+
+    def test_seas_approved(self):
+        response = self.client.get('/home/seas/approved')
+        self.assertEqual(response.status_code, 200)
+
+    def test_seas_message_post(self):
+        response = self.client.post(path='/home/seas/post')
+        msgs = Message.objects.all()
+        self.assertEqual(response.status_code, 302)
+        #print(msgs)
+        self.assertEqual(len(msgs), 1)
+
+    #returning 404, unsure why
+    # def test_seas_search(self):
+    #     response = self.client.post('home/seas/search', {'search': 'APMA'})
+    #     offerings = Offering.objects.all()
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertGreater(len(offerings), 0)
+
 
