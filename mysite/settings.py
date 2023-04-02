dd011a62b8c647aa289a73d46a5c29fcff7d8c57
zjmills <zjm7xbn@virginia.edu>
2023-04-02 14:40:55 -0400

    home page

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1','project-b29.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
